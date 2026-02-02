3d87893728fff63126c709e1092cfa0e1947302e
Cristina <cris.glez512@gmail.com>
2026-02-02 14:10:27 +0100

    Soluciono error tabla PARTE 2 DE SEÑOR ANILLOS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(),
     providePrimeNG({
             theme: {
                 preset: Aura
--- src/app/personajes/buscar-personaje/buscar-personaje.ts
+++ src/app/personajes/buscar-personaje/buscar-personaje.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PersonajesService } from '../../servicios/personajes-service';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buscar-personaje',
-  imports: [ButtonModule, FormsModule, RouterLink],
+  imports: [ButtonModule, FormsModule, CommonModule],
   templateUrl: './buscar-personaje.html',
   styleUrl: './buscar-personaje.css',
 })
@@ -15,7 +16,11 @@ export class BuscarPersonaje implements OnInit {
   personajes: any [] = []
   error = ''
 
-  constructor(private personajeService: PersonajesService, private router: Router){}
+  constructor(
+    private personajeService: PersonajesService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ){}
 
   ngOnInit(): void
   {
@@ -27,6 +32,8 @@ export class BuscarPersonaje implements OnInit {
     this.personajeService.obtenerPersonajes().subscribe({
       next: (data) => {
         this.personajes = data
+        this.cdr.detectChanges()
+        console.log(data)
       }, error: (err) => {
         this.error = 'Se ha producido un error'
       }
